494f7b08323756ee4c6910eaeda720c8859c90cc
vladosG005 <vladosG005@yandex.ru>
2025-05-26 14:26:12 +0300

    Update lab2.js: add documentation to functions

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,5 +1,12 @@
 'use strict';
 
+/*
+ * Возвращает x в степени целого числа n.
+ *
+ * @param {number} x - возводимое в степень число.
+ * @param {number} n - степень, должна быть целым числом.
+ * @return {number} NaN, если n - нецелое или x = 0 и n = 0, в остальных случаях x в степени n.
+*/
 function pow(x, n) {
 
     if (Math.floor(n) != n) {
@@ -28,12 +35,30 @@ function pow(x, n) {
 
 }
 
-let sumTo = new Function('n', `let s = 0;
+/*
+ * Возвращает сумму всех натуральных чисел от 1 до n.
+ *
+ * @param {number} n - максимальное натуральное число, входящее в сумму.
+ * @return {number} NaN, если n - не натуральное, в остальных случаях сумму чисел от 1 до n.
+*/
+let sumTo = new Function('n', `if (Math.floor(n) != n || n < 1) {
+        return NaN;
+    }
+    
+    let s = 0;
     for (let i = 1; i <= n; i++) {
         s += i;
     }
+    
     return s;`);
 
+/*
+ * Определяет, високосный ли год под номером year.
+ * Високосным считается год, делящийся на 4, но не делящийся на 100, или делящийся на 400.
+ *
+ * @param {number} year - номер года.
+ * @return {boolean} true, если year соответствует високосному году, а в остальных случаях - false.
+*/
 function isLeapYear(year) {
 
     return year % 4 == 0 ?
@@ -45,7 +70,17 @@ function isLeapYear(year) {
         
 }
 
+/*
+ * Возвращает факториал n.
+ *
+ * @param {number} n - неотрицательное число.
+ * @return {BigInt|number} NaN, если n - отрицательное или дробное, в остальных случаях - факториал n.
+*/
 function factorial(n) {
+
+    if (Math.floor(n) != n || n < 0) {
+        return NaN;
+    }
 
     if (n == 0n) {
         return 1n;
@@ -56,22 +91,33 @@ function factorial(n) {
 
 }
 
+/*
+ * Возвращает число Фибоначчи под номером n.
+ * Считается, что fib(0) = 0 и fib(1) = 1.
+ *
+ * @param {number} n - целое число.
+ * @return {number|BigInt} NaN, если n - нецелое, в остальных случаях - соответствующее число Фибоначчи.
+*/
 function fib(n) {
-    
+	
+    if (Math.floor(n) != n) {
+        return NaN;
+    }
+	
     let x1 = -1n;
     let x = 0n;
     let t = 0n;
     
-	let i = 0;
-	if (n != 0) {
+    let i = 0;
+    if (n != 0) {
         // Количество знаков в двоичной записи
-	    i = Math.ceil(Math.log2(Math.abs(n)));
-	}
+	i = Math.ceil(Math.log2(Math.abs(n)));
+    }
     
     while (i--) {
 
         // Удвоение номера числа Фибоначчи
-		t = x * (2n * x1 + x);
+	t = x * (2n * x1 + x);
         x1 = x1 * x1 + x * x;
         x = t;
 
@@ -90,8 +136,18 @@ function fib(n) {
     
 }
 
+/*
+ * Возвращает функцию сравнения с x.
+ * Возвращаемая функция возвращает true, если её аргумент больше x, null, если он равен x, и false, если он меньше x.
+ * @param {number} x - целое число.
+ * @return {null|Function} null, если x - нецелое, в остальных случаях - вышеописанную функцию.
+*/
 function compare(x) {
-
+	
+    if (Math.floor(n) != n) {
+        return null;
+    }
+	
     return function(y) {
 
         return y > x ? true
@@ -102,6 +158,11 @@ function compare(x) {
 
 }
 
+/*
+ * Возвращает сумму передаваемых чисел.
+ * @param {Array} ...args - массив чисел.
+ * @return {number} сумму чисел из ...args.
+*/
 function sum(...args) {
 
     let s = 0;
@@ -114,6 +175,11 @@ function sum(...args) {
 
 }
 
+/*
+ * Возвращает объект с добавленным скрытым свойством blackSpot со значением true.
+ * @param {object} obj - объект.
+ * @return {object} тот же объект obj, но с новым свойством blackSpot.
+*/
 function addBlackSpot(obj) {
 	
     obj[Symbol.for('blackSpot')] = true;
