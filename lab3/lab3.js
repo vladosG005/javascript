e73c96472887762384ae0baa045f0f035b5e14ab
vladosG005 <vladosG005@yandex.ru>
2025-05-29 11:40:04 +0300

    Update lab3.js: do function 1

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -1,4 +1,9 @@
-
-function getDecimal(num) {
+/*
+ * Возвращает дробную часть числа, т.е. какое положительное число, меньшее 1, необходимо вычесть, чтобы получить целое число.
+ * @param {number} num - исходное число.
+ * @return {number} дробную часть num.
+*/
+export function getDecimal(num) {
     a = 5;
+    return num - Math.floor(num);
 }
